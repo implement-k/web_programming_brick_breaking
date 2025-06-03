04be9ba6311916efde9188c7e3af914bc796a72a
implement-k <rnjsrn98@gmail.com>
2025-06-03 16:46:42 +0900

    miniGame start

--- main.js
+++ main.js
@@ -22,105 +22,10 @@ $(document).ready(function () {
         mainGame.init();
     } 
     
-    // 임시: 보스전 시작버튼
-    $('#tmp_boss_start').click(() => {
-        gameStarted = false;
-        $('.clear').hide();
-        bossGame.init(gameDifficulty);
+    $('#miniGameBtn').click(() => {
+        miniGameStart();
     });
-    // 임시: 게임 시작
-    $('#tmp_game_start').click(() => {
-        if (gameDifficulty < 3) {
-            startMainGame(gameDifficulty);
-        } else if (gameDifficulty === 3) {
-            startMainGame(gameDifficulty);
-        } else if (gameDifficulty === 4) {
-            showScene('title-screen');
-            hideScene('main-game');
-        }
-        /*
-        $('#gameCanvas').show();
-        mainGame.gameStarted = true;
-        $(this).hide();
-        mainGame.draw();
-        */
-
-        // 기존 마스터 버튼들 제거
-        $('#masterBtns').empty();
-
-        // 마스터 버튼들
-        let btn1 = $('<button/>');
-        btn1.text('블록 다 깨기');
-        btn1.click(() => {
-            // 현재 블록에 있는 모든 광물들 수집
-            mainGame.collectAllItems();
-            mainGame.isClear = true;
-        });
-        $('#masterBtns').append(btn1);
-        let btn2 = $('<button/>');
-        btn2.text('죽기');
-        btn2.click(() => {
-            // 안전한 방식으로 체력을 0으로 설정
-            if (user && user.heart) {
-                user.heart.health = 0;
-                user.hit(1);
-            }
-        });
-        $('#masterBtns').append(btn2);
-        
-        let btn3 = $('<button/>');
-        btn3.text('보스 죽이기');
-        btn3.click(() => {
-            if (bossGame && bossGame.bossManager && bossGame.bossManager.curBoss) {
-                bossGame.bossManager.curBoss.health = 1;
-                bossGame.bossManager.curBoss.isDying = true;
-                bossGame.bossManager.curBoss.dropItem();
-                bossGame.bossManager.curBoss.deathSound.play();
-            }
-        });
-        $('#masterBtns').append(btn3);
-    });
-    // 시작 버튼 처리
-    $('#startButton').click(function() {
-        $('#gameCanvas').show();
-        mainGame.gameStarted = true;
-        $(this).hide();
-        mainGame.draw();
-
-        // 기존 마스터 버튼들 제거
-        $('#masterBtns').empty();
-
-        // 마스터 버튼들
-        let btn1 = $('<button/>');
-        btn1.text('블록 다 깨기');
-        btn1.click(() => {
-            // 현재 블록에 있는 모든 광물들 수집
-            mainGame.collectAllItems();
-            mainGame.isClear = true;
-        });
-        $('#masterBtns').append(btn1);
-        let btn2 = $('<button/>');
-        btn2.text('죽기');
-        btn2.click(() => {
-            // 안전한 방식으로 체력을 0으로 설정
-            if (user && user.heart) 
-                user.heart.health = 0;
-            
-        });
-        $('#masterBtns').append(btn2);
-        
-        let btn3 = $('<button/>');
-        btn3.text('보스 죽이기');
-        btn3.click(() => {
-            if (bossGame && bossGame.bossManager && bossGame.bossManager.curBoss) {
-                bossGame.bossManager.curBoss.health = 1;
-                bossGame.bossManager.curBoss.isDying = true;
-                bossGame.bossManager.curBoss.dropItem();
-                bossGame.bossManager.curBoss.deathSound.play();
-            }
-        });
-        $('#masterBtns').append(btn3);
-    });
+   
     // 리스폰 버튼 처리
     $('#respawn').click(() => {
         let preserveUser = false;
@@ -150,3 +55,8 @@ $(document).ready(function () {
         bossGame = new BossGame(gameDifficulty);  // 보스 게임 초기화
     });
 });
+
+function miniGameStart() {
+    console.log('미니 게임 시작');
+    
+}
